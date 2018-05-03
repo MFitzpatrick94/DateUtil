d10511113b9f04fff059b156134ba9405504ab7e
MFitzpatrick94 <rasberrysoc@hotmail.com>
2018-05-03 14:31:00 -0500

    DateTime

--- src/com/arkansascodingacademy/TimeUtil.java
+++ src/com/arkansascodingacademy/TimeUtil.java
@@ -8,16 +8,22 @@ public class TimeUtil
     //Return true if it is after 12PM and before 4PM
     public static boolean isAfternoon(LocalTime time)
     {
-        boolean afternoon = false;
 
-        return afternoon;
+        if(time.isAfter(LocalTime.of(12,00)) && time.isBefore(LocalTime.of(16,0)))
+        {
+            return true;
+        }
+
+       else return false;
     }
 
     //Return true if it is after midnight and before 4AM
     public static boolean isAfterMidnight(LocalTime time)
     {
-        boolean afterMidnight = false;
-
-        return afterMidnight;
+        if(time.isAfter(LocalTime.of(0,00)) && time.isBefore(LocalTime.of(4,0)))
+        {
+            return true;
+        }
+        else return false;
     }
 }
