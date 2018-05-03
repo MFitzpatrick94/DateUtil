593150ae5b9295c40ecdd79b26204c723dd3e314
MFitzpatrick94 <rasberrysoc@hotmail.com>
2018-05-03 09:45:12 -0500

    DateTime

--- test/com/arkansascodingacademy/DateTimeUtilTest.java
+++ test/com/arkansascodingacademy/DateTimeUtilTest.java
@@ -2,6 +2,8 @@ package com.arkansascodingacademy;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DateTimeUtilTest
@@ -10,6 +12,19 @@ class DateTimeUtilTest
     @Test
     void isHappyHour()
     {
+        LocalDateTime fridayNotHappyHour = LocalDateTime.of(2018,5,4,8,0);
+        LocalDateTime fridayHappyHour = LocalDateTime.of(2018,5,4,17,0);
+        LocalDateTime thursdayHappyHour = LocalDateTime.of(2018,5,3,17,0);
+        LocalDateTime thursdayNotHappyHour = LocalDateTime.of(2018,5,3,12,0);
+        LocalDateTime saturday = LocalDateTime.of(2018,5,5,17,0);
+        assertFalse(DateTimeUtil.isHappyHour(fridayNotHappyHour));
+        assertTrue(DateTimeUtil.isHappyHour(fridayHappyHour));
+        assertFalse(DateTimeUtil.isHappyHour(thursdayNotHappyHour));
+        assertFalse(DateTimeUtil.isHappyHour(saturday));
+        assertTrue(DateTimeUtil.isHappyHour(thursdayHappyHour));
+
+
+
 
     }
 }
